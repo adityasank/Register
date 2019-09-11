67b943ee4506ea28916cdd0b09d249d3d5334c99
aditya das <adityasank87@gmail.com>
2019-09-11 14:41:13 +0530

    first

---
+++ src/views/Authentication/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <vs-row vs-justify="center">
+  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+    <vs-card>
+        <vs-row>
+  <vs-col  vs-type="flex" vs-justify="center" vs-align="center" vs-w="6">
+    
+        
+      
+  </vs-col>
+  <vs-col vs-w="6">
+  <div>
+      <h1> Login </h1>
+      
+      <h3> Welcome Back Please Login To your Account </h3>
+       <vs-input class="inputx " vs-type="flex" placeholder="Placeholder" v-model="value1"/>
+       </div>
+      </vs-col>
+</vs-row>
+      
+    </vs-card>
+  </vs-col>
+</vs-row>
+</template>
